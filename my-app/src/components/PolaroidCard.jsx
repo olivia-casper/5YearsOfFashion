f509826aa23a755e1af1305490a3193483e22775
olivia-casper <oliviaanncasper@gmail.com>
2025-04-06 20:37:51 -0400

    toggle heart

--- my-app/src/components/PolaroidCard.jsx
+++ my-app/src/components/PolaroidCard.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import "./css/PolaroidCard.css";
 
 function PolaroidCard({ imgSrc, alt, caption, className }) {
+  const [liked, setLiked] = useState(false);
+
+  const toggleLike = () => {
+    setLiked(!liked);
+  };
+
   return (
     <div className={`polaroid ${className}`}>
       <img src={imgSrc} alt={alt} />
       <div className="caption">
         <p>{caption}</p>
-        <div className="heart">♡</div>
+        <div
+          className={`heart ${liked ? "liked" : ""}`}
+          onClick={toggleLike}
+        >
+          {liked ? "♥" : "♡"}
+        </div>
       </div>
     </div>
   );
